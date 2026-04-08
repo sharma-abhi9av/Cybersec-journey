1ae21dfbb5edf7f6ed0366ae431b821ea2e6b770
sharma-abhi9av <in.abhi.linked@gmail.com>
2026-04-08 17:24:22 +0530

    Improve longestCommonPrefix implementation
    Refactor longestCommonPrefix method for clarity and efficiency.

--- coding/leetcode_python/Longest_common_prefix.py
+++ coding/leetcode_python/Longest_common_prefix.py
@@ -1,15 +1,15 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        # let's take the first word as reference
-        first_word = strs[0]
         # the input can be empty so,
         if not strs :
           return("")
+        # let's take the first word as reference
+        first_word = strs[0]
         # now for the puzzle --> we have the first word, let's first dig into it.
         for i, letter in enumerate(first_word):
-            print(letter)  # now we have output as "f, l, o, w, e, r"
+            # print(letter) now we have output as "f, l, o, w, e, r"
             # Next step should be checking the the letter of other words match.
-            for a, word in enumerate(strs):
-                if  a == len(word) or word[i] != first_word[i]:
+            for word in strs[1:]:
+                if  i == len(word) or word[i] != letter:
                     return(first_word[:i])
         return first_word 
