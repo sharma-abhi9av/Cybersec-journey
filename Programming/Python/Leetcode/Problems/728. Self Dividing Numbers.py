3fb6059ab990268cecb81f239f548b51721fa858
sharma-abhi9av <in.abhi.linked@gmail.com>
2026-05-08 00:09:27 +0530

    Update 728. Self Dividing Numbers.py

--- Programming/Python/Leetcode/Problems/728. Self Dividing Numbers.py
+++ Programming/Python/Leetcode/Problems/728. Self Dividing Numbers.py
@@ -1,12 +1,21 @@
+"""
+A self-dividing number is a number that is divisible by every digit it contains.
+
+    For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+
+A self-dividing number is not allowed to contain the digit zero.
+
+Given two integers left and right, return a list of all the self-dividing numbers in the range [left, right] (both inclusive).
+"""
 class Solution:
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
         output = []     
         for number in range(left,right+1):
-            a = True
+            status = True
             for digit in str(number):
                 if int(digit) ==0 or number % int(digit) !=0:
-                    a = False 
+                    status = False 
                     break
-            if a :
+            if status :
                 output.append(number)     
         return output
