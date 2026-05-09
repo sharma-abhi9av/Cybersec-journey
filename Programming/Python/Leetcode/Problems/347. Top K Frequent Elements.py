9346521740c5ee5c0f6a7d20973b24c00c073625
sharma-abhi9av <in.abhi.linked@gmail.com>
2026-05-09 15:26:23 +0530

    Replace topKFrequent with groupAnagrams method
    Removed topKFrequent method and added groupAnagrams method.

--- Programming/Python/Leetcode/Problems/347. Top K Frequent Elements.py
+++ Programming/Python/Leetcode/Problems/347. Top K Frequent Elements.py
@@ -2,18 +2,12 @@
 Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 """
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        output = []
-        hashmap={}
-        for num in nums : 
-            if num in hashmap:
-                hashmap[num] += 1
-            else:
-                hashmap[num] = 1
-        #print(hashmap)
-        sorted_hashmap = dict(sorted(hashmap.items(), key=lambda item: item[1], reverse=True))
-        #print(sorted_hashmap)
-        keys_list = list(sorted_hashmap.keys())
-        for i in range(k):
-            output.append(keys_list[i])
-        return(output)
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = []
+        output={}
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            if sorted_word not in output:
+                output[sorted_word]= []
+            output[sorted_word].append(word)
+        return list(output.values())
