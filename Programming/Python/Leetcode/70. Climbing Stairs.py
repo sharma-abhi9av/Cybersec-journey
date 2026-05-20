53789bde9c5f145cb847cb4cb31fa7bb7d62f0a3
sharma-abhi9av <in.abhi.linked@gmail.com>
2026-05-20 18:35:14 +0530

    Update 70. Climbing Stairs.py

--- Programming/Python/Leetcode/70. Climbing Stairs.py
+++ Programming/Python/Leetcode/70. Climbing Stairs.py
@@ -7,11 +7,13 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 
 Example 1:
 
+```
 Input: n = 2
 Output: 2
 Explanation: There are two ways to climb to the top.
 1. 1 step + 1 step
 2. 2 steps
+```
 
 # Thinking Process and Diagram 
 ![70. Climbing Stairs](pictures/70_climbing_stairs.png)
