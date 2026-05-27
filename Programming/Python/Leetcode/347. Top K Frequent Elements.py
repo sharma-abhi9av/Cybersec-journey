7f4551c8e6d815afdb2779e2677078317b5a258b
Abhinav Sharma <in.abhi.linked@gmail.com>
2026-05-27 21:52:53 +0530

    Remove commented print statements and clean up code

--- Programming/Python/Leetcode/347. Top K Frequent Elements.py
+++ Programming/Python/Leetcode/347. Top K Frequent Elements.py
@@ -10,9 +10,7 @@ class Solution:
                 hashmap[num] += 1
             else:
                 hashmap[num] = 1
-        #print(hashmap)
         sorted_hashmap = dict(sorted(hashmap.items(), key=lambda item: item[1], reverse=True))
-        #print(sorted_hashmap)
         keys_list = list(sorted_hashmap.keys())
         for i in range(k):
             output.append(keys_list[i])
