23b14d3dd3cf12aeaf99623c679a2822709f99d9
Abhinav Sharma <in.abhi.linked@gmail.com>
2026-05-27 21:52:15 +0530

    Refine docstring for pivot integer problem
    Updated the docstring to clarify the problem statement.

--- Programming/Python/Leetcode/2485. Find the Pivot Integer.py
+++ Programming/Python/Leetcode/2485. Find the Pivot Integer.py
@@ -1,11 +1,7 @@
 """
 Given a positive integer n, find the pivot integer x such that:
-
-    The sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively.
-
-Return the pivot integer x. If no such integer exists, return -1. It is guaranteed that there will be at most one pivot index for the given input.
-
-
+- The sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively.
+- Return the pivot integer x. If no such integer exists, return -1. It is guaranteed that there will be at most one pivot index for the given input.
 """
 class Solution:
     def pivotInteger(self, n: int) -> int:
