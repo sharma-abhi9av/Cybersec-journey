21561dd468473aa25b65887aadf1632dbd322a7f
sharma-abhi9av <in.abhi.linked@gmail.com>
2026-06-05 10:36:07 +0530

    add htb stats dashboard and engine

--- Programming/C/CS50/addresses.c
+++ Programming/C/CS50/addresses.c
@@ -1,6 +1,12 @@
 #include <stdio.h> 
 int main(void){
     int n = 50;
+    int *p = &n; 
     printf("%i\n",n);       // print the number 
     printf("*%p\n", &n) ;   // print the address of number in memory 
+<<<<<<< Updated upstream
 }
+=======
+    printf("%i\n", *p);
+}
+>>>>>>> Stashed changes
