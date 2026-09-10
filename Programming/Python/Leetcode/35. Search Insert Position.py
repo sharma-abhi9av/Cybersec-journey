0c449a397979826257426dfd928cfb8ac1ffc6a8
sharma-abhi9av <in.abhi.linked@gmail.com>
2026-09-11 00:12:33 +0530

    Revisited the problem, removing rust

--- Programming/Python/Leetcode/35. Search Insert Position.py
+++ Programming/Python/Leetcode/35. Search Insert Position.py
@@ -4,14 +4,14 @@ You must write an algorithm with O(log n) runtime complexity.
 """
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        left =0 
+        left =0
         right = len(nums) -1
         while left <= right :
-            mid = (left + right) //2 
+            mid = (left + right) //2
             if nums[mid] > target:
                 right = mid-1
             elif nums[mid] < target:
-                left = mid +1 
-            else : 
-                return mid 
-        return left  
+                left = mid +1
+            else :
+                return mid
+        return left
